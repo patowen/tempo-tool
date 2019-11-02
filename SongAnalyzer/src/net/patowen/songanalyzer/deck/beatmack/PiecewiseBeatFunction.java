b911eb7eb9f72f10613a501ee9352b710dd9a94d
Patrick Owen <patowen95@gmail.com>
2019-11-02 10:20:00 -0400

    Add idea for better tempo UX

--- SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/PiecewiseBeatFunction.java
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/PiecewiseBeatFunction.java
@@ -22,4 +22,14 @@ public class PiecewiseBeatFunction implements BeatFunction {
  * Use cases of tempo variance:
  * Uneven tempo due to imperfection
  * Sudden tempo changes or tempo ramps for effect
+ * 
+ * 
+ * Idea (for when there are no constant tempo sections):
+ * Let tempo be piecewise-linear with user defined marks as knots
+ * Workflow:
+ * Drag right where tick sounds are too early
+ * Drag left where tick sounds are too late
+ * Fix phase at points, NOT TEMPO
+ * Think about phase function during implementation, not tempo function
+ * Use cubic interpolation. Don't do anything crazy for extrapolation.
  */
