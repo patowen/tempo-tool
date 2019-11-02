36a3d12228b45da5b54653418ebf4fe0bb613873
Patrick Owen <patowen95@gmail.com>
2019-11-02 17:39:43 -0400

    First draft of natural cubic interpolation

---
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/Spline.java
@@ -0,0 +1,66 @@
+package net.patowen.songanalyzer.deck.beatmack;
+
+import java.util.Arrays;
+
+// Ostensibly immutable; recreated when knots move
+// Use linear extrapolation at endpoints
+public class Spline {
+	private final int numRegions;
+	
+	public final double[] x;
+	public final double[] y;
+	
+	public final double[] a;
+	public final double[] b;
+	
+	private final TridiagonalSystem system;
+	
+	public Spline(int numRegions) {
+		this.numRegions = numRegions;
+		
+		x = new double[numRegions + 1];
+		y = new double[numRegions + 1];
+		a = new double[numRegions];
+		b = new double[numRegions];
+		
+		system = new TridiagonalSystem(numRegions + 1);
+	}
+	
+	// Precondition: system of equations is zeroed
+	public void computeSpline() {
+		for (int i=0; i<numRegions; i++) {
+			double coeffTerm = 1.0 / (x[i+1] - x[i]);
+			
+			system.a[i+1] += coeffTerm;
+			system.b[i] += 2 * coeffTerm;
+			system.b[i+1] += 2 * coeffTerm;
+			system.c[i] += coeffTerm;
+			
+			double constTerm = 3 * (y[i+1] - y[i]) * (coeffTerm * coeffTerm);
+			
+			system.d[i] += constTerm;
+			system.d[i+1] += constTerm;
+		}
+		
+		system.solve();
+		
+		for (int i=0; i<numRegions; i++) {
+			a[i] = system.x[i] * (x[i+1] - x[i]) - (y[i+1] - y[i]);
+			b[i] = -system.x[i+1] * (x[i+1] - x[i]) + (y[i+1] - y[i]);
+		}
+	}
+	
+	public double eval(double xVal) {
+		// TODO: Extrapolation
+		int i = Arrays.binarySearch(x, xVal);
+		if (i >= 0) {
+			return y[i];
+		}
+		i = -i - 1;
+		
+		double t = (xVal - x[i]) / (x[i+1] - x[i]);
+		double s = 1 - t;
+		
+		return s*y[i] + t*y[i+1] + t*s*(s*a[i] + t*b[i]);
+	}
+}
---
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/TridiagonalSystem.java
@@ -0,0 +1,50 @@
+package net.patowen.songanalyzer.deck.beatmack;
+
+public class TridiagonalSystem {
+	public final int size;
+	
+	public final double[] a;
+	public final double[] b;
+	public final double[] c;
+	public final double[] d;
+	
+	private final double[] c2;
+	private final double[] d2;
+	
+	public final double[] x;
+	
+	public TridiagonalSystem(int size) {
+		this.size = size;
+		
+		a = new double[size];
+		b = new double[size];
+		c = new double[size];
+		d = new double[size];
+		
+		c2 = new double[size];
+		d2 = new double[size];
+		
+		x = new double[size];
+	}
+	
+	public void solve() {
+		if (size == 0) {
+			return; // Nothing to do
+		}
+		
+		// Forward sweep (Note: c2[n-1] will be set but unused)
+		c2[0] = c[0] / b[0];
+		d2[0] = d[0] / b[0];
+		
+		for (int i=1; i<size; i++) {
+			c2[i] = c[i] / (b[i] - a[i]*c2[i-1]);
+			d2[i] = (d[i] - a[i]*d2[i-1]) / (b[i] - a[i]*c2[i-1]);
+		}
+		
+		// Back substitution
+		x[size-1] = d2[size-1];
+		for (int i=size-2; i>=0; i--) {
+			x[i] = d2[i] - c2[i]*x[i+1];
+		}
+	}
+}
