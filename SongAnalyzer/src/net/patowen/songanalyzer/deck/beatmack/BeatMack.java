050ddd29241e51f3b9caf0ebe4424d410315c482
Patrick Owen <patowen95@gmail.com>
2019-11-02 23:26:43 -0400

    Allow the BeatMack to render its computed beats

--- SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMack.java
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMack.java
@@ -3,7 +3,6 @@ package net.patowen.songanalyzer.deck.beatmack;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.geom.Ellipse2D;
 
 import net.patowen.songanalyzer.bundle.DeckBundle;
 import net.patowen.songanalyzer.data.Dict;
@@ -25,22 +24,14 @@ public class BeatMack extends Mack {
 		trackBounds = bundle.trackBounds;
 		
 		spline = new Spline(2);
-		spline.x[0] = -1;
-		spline.y[0] = 0.5;
-		spline.x[1] = 0;
-		spline.y[1] = 0;
-		spline.x[2] = 3;
+		spline.x[0] = 1;
+		spline.y[0] = 0;
+		spline.x[1] = 2;
+		spline.y[1] = 0.5;
+		spline.x[2] = 5;
 		spline.y[2] = 3;
 		
 		spline.computeSpline();
-	}
-	
-	private double getPixelY(double realY) {
-		return (double)height * (1.0 - (realY + 1.0) / 5.0);
-	}
-	
-	private void drawCircle(Graphics2D g, double x, double y, double r) {
-		g.fill(new Ellipse2D.Double(x - r, y - r, r * 2, r * 2));
 	}
 
 	@Override
@@ -50,21 +41,42 @@ public class BeatMack extends Mack {
 
 	@Override
 	public void render(Graphics2D g) {
-		g.setColor(Color.GREEN);
-		for (int i=0; i<spline.x.length; i++) {
-			double x = trackBounds.secondsToSubpixel(spline.x[i]);
-			double y = getPixelY(spline.y[i]);
-			drawCircle(g, x, y, 5);
+		renderBeats(g);
+	}
+	
+	private void renderBeats(Graphics2D g) {
+		g.setColor(Color.GRAY);
+		double currentPhase = spline.eval(trackBounds.subpixelToSeconds(0));
+		for (int i=0; i<width-1; i++) {
+			double nextPhase = spline.eval(trackBounds.subpixelToSeconds(i+1));
+			if (Math.ceil(currentPhase) < Math.ceil(nextPhase)) {
+				g.drawLine(i, 0, i, height-1);
+			}
+			currentPhase = nextPhase;
 		}
-		
+	}
+	
+	@SuppressWarnings("unused")
+	private void renderPhaseGraph(Graphics2D g) {
 		g.setColor(Color.CYAN);
-		for (int pixelX = 0; pixelX < width; pixelX ++) {
-			double realX = trackBounds.pixelToSeconds(pixelX);
-			double realY = spline.eval(realX);
-			int pixelY = (int)Math.floor(0.5 + getPixelY(realY));
-			
-			g.drawLine(pixelX, pixelY, pixelX, pixelY);
+		double phasePrev = spline.eval(trackBounds.pixelToSeconds(-1));
+		double phaseBase = Math.floor(phasePrev);
+		for (int pixelX = 0; pixelX <= width; pixelX++) {
+			double phase = spline.eval(trackBounds.pixelToSeconds(pixelX));
+			while (true) {
+				g.drawLine(pixelX - 1, phaseToPixel(phasePrev - phaseBase), pixelX, phaseToPixel(phase - phaseBase));
+				if (phase - phaseBase > 1) {
+					phaseBase += 1;
+				} else {
+					break;
+				}
+			}
+			phasePrev = phase;
 		}
+	}
+	
+	private int phaseToPixel(double phase) {
+		return height - 1 - (int)(Math.floor(phase * height));
 	}
 
 	@Override
