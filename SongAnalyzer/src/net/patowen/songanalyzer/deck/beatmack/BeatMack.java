3a02dfdcf5b43564794b9c4bc8b28376817f81c3
Patrick Owen <patowen95@gmail.com>
2019-11-03 14:36:31 -0500

    Allow user to click and drag knots

--- SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMack.java
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMack.java
@@ -3,6 +3,8 @@ package net.patowen.songanalyzer.deck.beatmack;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
 
 import net.patowen.songanalyzer.bundle.DeckBundle;
 import net.patowen.songanalyzer.data.Dict;
@@ -10,17 +12,29 @@ import net.patowen.songanalyzer.data.FileFormatException;
 import net.patowen.songanalyzer.deck.Mack;
 import net.patowen.songanalyzer.deck.TrackBounds;
 import net.patowen.songanalyzer.userinput.InputAction;
+import net.patowen.songanalyzer.userinput.InputActionDrag;
+import net.patowen.songanalyzer.userinput.InputDictionary;
+import net.patowen.songanalyzer.userinput.InputMapping;
 import net.patowen.songanalyzer.userinput.InputType;
+import net.patowen.songanalyzer.userinput.InputTypeMouse;
 import net.patowen.songanalyzer.userinput.MouseHoverFeedback;
 
 public class BeatMack extends Mack {
 	public static final int type = 2;
 	
+	private InputDictionary inputDictionary;
+	
 	private final TrackBounds trackBounds;
 	
 	private Spline spline;
 	
+	private int selectionRange = 3;
+	
 	public BeatMack(DeckBundle bundle) {
+		inputDictionary = new InputDictionary();
+		inputDictionary.addInputMapping(new InputMapping(new MoveKnotAtMouse(), new InputTypeMouse(MouseEvent.BUTTON1, false, false, false), 1));
+		inputDictionary.constructDictionary();
+		
 		trackBounds = bundle.trackBounds;
 		
 		spline = new Spline(2);
@@ -92,7 +106,7 @@ public class BeatMack extends Mack {
 
 	@Override
 	public InputAction applyInputAction(InputType inputType, Point mousePos, double value) {
-		return null;
+		return inputDictionary.applyInput(inputType, mousePos, value);
 	}
 
 	@Override
@@ -106,5 +120,58 @@ public class BeatMack extends Mack {
 
 	@Override
 	public void load(Dict dict) throws FileFormatException {
+	}
+	
+	private class MoveKnotAtMouse implements InputActionDrag {
+		private int knot;
+		private double initialX;
+		
+		@Override
+		public boolean onAction(Point pos, double value) {
+			if (isWithinView(pos)) {
+				double seconds = trackBounds.pixelToSeconds(pos.x);
+				int index = Arrays.binarySearch(spline.x, seconds);
+				
+				int lowerIndex = (index < 0) ? -index - 2 : index;
+				int upperIndex = lowerIndex + 1;
+				
+				int closestIndex;
+				if (lowerIndex < 0) {
+					closestIndex = upperIndex;
+				} else if (upperIndex >= spline.x.length) {
+					closestIndex = lowerIndex;
+				} else {
+					if (seconds - spline.x[lowerIndex] < spline.x[upperIndex] - seconds) {
+						closestIndex = lowerIndex;
+					} else {
+						closestIndex = upperIndex;
+					}
+				}
+				
+				int knotPixelX = trackBounds.secondsToPixel(spline.x[closestIndex]);
+				if (pos.x >= knotPixelX - selectionRange && pos.x <= knotPixelX + selectionRange) {
+					knot = closestIndex;
+					initialX = spline.x[closestIndex];
+					return true;
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public void onDrag(Point startRelative) {
+			spline.x[knot] = trackBounds.subpixelToSeconds(trackBounds.secondsToSubpixel(initialX) + startRelative.x);
+			spline.computeSpline();
+		}
+
+		@Override
+		public void onCancel() {
+			spline.x[knot] = initialX;
+			spline.computeSpline();
+		}
+
+		@Override
+		public void onEnd(Point startRelative) {
+		}
 	}
 }
