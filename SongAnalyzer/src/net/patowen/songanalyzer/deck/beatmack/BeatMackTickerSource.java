2ed61623e72a99ce61549b432e7eb3af74a246f0
Patrick Owen <patowen95@gmail.com>
2019-11-03 00:03:31 -0400

    Simplify and fix BeatMapTickerSource

--- SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMackTickerSource.java
+++ SongAnalyzer/src/net/patowen/songanalyzer/deck/beatmack/BeatMackTickerSource.java
@@ -1,19 +1,10 @@
 package net.patowen.songanalyzer.deck.beatmack;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import net.patowen.songanalyzer.TickerSource;
 
 public class BeatMackTickerSource implements TickerSource {
 	// TODO: Point to something that won't get replaced
 	private Spline spline;
-	
-	// TODO: Invalidate cache when spline is adjusted
-	
-	// Note: The cache allows consistency to avoid any beats from being doubled
-	// or missed when played back
-	private Map<Integer, Double> beatToTimeCache = new HashMap<>();
 	
 	public BeatMackTickerSource(Spline spline) {
 		this.spline = spline;
@@ -21,36 +12,26 @@ public class BeatMackTickerSource implements TickerSource {
 	
 	@Override
 	public Double getNextTickInclusive(double pos) {
-		return getNextTick(pos, true);
+		double phase = spline.eval(pos);
+		if (phase == Math.floor(phase)) {
+			return pos;
+		}
+		return getNextTickExclusive(pos);
 	}
 	
 	@Override
 	public Double getNextTickExclusive(double pos) {
-		return getNextTick(pos, false);
-	}
-	
-	private Double getNextTick(double pos, boolean inclusive) {
 		double phase = spline.eval(pos);
-		int minNextBeat = (int)Math.floor(phase);
-		Double minNextTick = beatToTimeCache.get(minNextBeat);
-		if (minNextTick != null && (minNextTick > pos || (inclusive && minNextTick == pos))) {
-			return minNextTick;
-		}
-		
-		return null;
+		return getTimeForPhase(Math.floor(phase + 1), pos);
 	}
 	
-	public Double getTimeForBeat(int beat, double guess) {
-		Double cachedTime = beatToTimeCache.get(beat);
-		if (cachedTime != null) {
-			return cachedTime;
-		}
-		
+	public Double getTimeForPhase(double phase, double guess) {
 		for (int i=0; i<10; i++) {
-			double newGuess = guess - spline.eval(guess) / spline.derivative(guess);
+			double newGuess = guess - (spline.eval(guess) - phase) / spline.derivative(guess);
 			if (newGuess == guess) { // TODO: Consider allowing more error
 				return guess;
 			}
+			guess = newGuess;
 		}
 		throw new RuntimeException("Newton's method failed to converge in time");
 	}
